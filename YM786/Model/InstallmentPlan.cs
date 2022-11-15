ee38d088aa240b95adaa5521d16ebabb9d276625
Asus <Asus@LAPTOP-29CNCDPO>
2022-11-15 17:39:08 +0500

    Добавьте файлы проекта.

---
+++ YM786/Model/InstallmentPlan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+
+namespace YM786.Model
+{
+    enum ProductСategories
+    {
+        Smartphone = 1,
+        Television = 2,
+        Computer = 3
+    }
+    class InstallmentPlan 
+    {   
+        int[] range = { 3, 6, 9, 12, 18, 24 };
+        private int _range;
+        public int CheckRenge 
+        {
+            get => _range;
+            set
+            {
+                if (value >= 3 && value.Equals(range.FirstOrDefault(x => x.Equals(value))))
+                    _range = value;
+                else
+                    throw new Exception("Range set incorrectly!! ");
+            }
+        }
+        public int PrincipalAmount { get; set; }
+        public string SMSmessage { get; set; }
+        public int Percent { get; set; }
+
+    }
+    class InstallmentPlanManager : InstallmentPlan
+    {
+        public int PaymentCalculation(ProductСategories product, int amountOfPayment, int phoneNumber, int range)
+        {
+            
+            
+            switch (product)
+            {
+                case ProductСategories.Smartphone:
+                    Percent = 3 * range / 12; 
+                    PrincipalAmount = (amountOfPayment * Percent) / 100 + amountOfPayment;
+                    break;
+                case ProductСategories.Computer:
+                    Percent = 3 * range / 12;
+                    PrincipalAmount = (amountOfPayment * Percent) / 100 + amountOfPayment;
+                    break;
+                case ProductСategories.Television:
+                    Percent = 3 * range / 18;
+                    PrincipalAmount = (amountOfPayment * Percent) / 100 + amountOfPayment;
+                    break;
+                default:
+                    throw new Exception("Wrong categoria type!");
+            }
+            SMSmessage = $"Phone number {phoneNumber},monthly payment: {Percent}, due date: {range} and commission{PrincipalAmount- amountOfPayment}";
+            return PrincipalAmount;
+        }
+    }
+}
---
+++ YM786/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using YM786.Model;
+
+namespace YM786
+{
+    class Program
+    {   
+        private static string _connectoinString = ConfigurationManager.ConnectionStrings["StoreDB"].ConnectionString;
+
+        private static SqlConnection _sqlConnection = null;
+        static void Main(string[] args)
+        {
+            //подключение к базам данных
+            _sqlConnection = new SqlConnection(_connectoinString);
+            _sqlConnection.Open();
+
+            Console.WriteLine("StoreApp");
+            string command = string.Empty;
+            //Объект для запуска любых команд
+            SqlCommand sqlCommand;
+            while (true)
+            {
+                try
+                {
+                    InstallmentPlanManager installmentPlan = new InstallmentPlanManager();
+                    Console.Write("Phone Number: ");
+                    int phoneNumber = int.Parse(Console.ReadLine());
+                    Console.Write("Amount: ");
+                    int amount = int.Parse(Console.ReadLine());
+                    Console.Write("Period: ");
+                    int renge = installmentPlan.CheckRenge = int.Parse(Console.ReadLine());
+                    Console.Write("Loan Type : ");
+                    string loanType = Console.ReadLine();
+                    //выйти и закрыть подключение базы данных
+                    if (loanType.ToLower().Equals("exit"))
+                    {
+                        if (_sqlConnection.State.Equals(ConnectionState.Open))
+                            _sqlConnection.Close();
+                        break;
+                    }
+                    int principalAmount = installmentPlan.PaymentCalculation((ProductСategories)Enum.Parse(typeof(ProductСategories), loanType), amount, phoneNumber, renge);
+                    Console.WriteLine(principalAmount);
+
+                    command = $"INSERT INTO [Table] (PhoneNumber" +
+                        $", PrincipalAmount, Period, LoanType) VALUES ('{phoneNumber}', '{principalAmount}', '{renge}', '{loanType}')";
+
+                    sqlCommand = new SqlCommand(command, _sqlConnection);
+
+                    Console.WriteLine(sqlCommand.ExecuteNonQuery());
+                    Console.ReadKey();
+                }
+                catch(Exception ex)
+                { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
